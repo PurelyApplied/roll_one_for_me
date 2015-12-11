4464f7e0622cc404399e2aa560ed9e50af51c4d1
Pat @ Raz <root@patrhom.com>
2015-12-11 20:07:07 +0000

    Added utility scripts.

---
+++ util.sh
@@ -0,0 +1,12 @@
+clear;
+echo "Roll one PID:"
+ps -A | grep py
+echo ""
+echo "Current contents of ./logs/"
+ls logs
+echo ""
+echo "Log tail without heartbeats:"
+grep -v "Heartbeat" rofm.log | tail -n20
+echo ""
+echo "Last of log:"
+tail -n3 rofm.log
