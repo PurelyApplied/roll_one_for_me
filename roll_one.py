b671cac0464bebf909f34b617412340977158cc9
Patrick S Rhomberg <patrick-rhomberg@uiowa.edu>
2016-01-11 12:08:54 -0600

    Classes moved to roll_one_classes.py; docstrings improved No functionality changes.

--- roll_one.py
+++ roll_one.py
@@ -3,23 +3,17 @@
 
 # To add: Look for tables that are actual tables.
 # Look for keyword ROLL in tables and scan for arbitrary depth
-import string, random, time, praw, re, pickle, os
-from pprint import pprint  #for debugging / live testing
+from roll_one_classes import *
+import string, time, praw, os
 
 def fdate():
     return "-".join(str(x) for x in time.gmtime()[:6])
 
-
-# Stash for future project:
-#   BtS = r.get_subreddit('DnDBehindTheScreen')
-#   mods = BtS.get_moderators()
-#   threes = [ mods[i:i+3] for i in range(0, len(mods), 3) ]
-
 ##################
 # Some constants #
 ##################
-_version="1.2.2"
-_last_updated="2015-12-02"
+_version="1.2.3"
+_last_updated="2016-01-11"
 
 _trash = string.punctuation + string.whitespace
 
@@ -38,6 +32,9 @@ _log = None
 _log_dir = "./logs"
 
 _trivial_passes_per_heartbeat = 30
+
+_seen_max_len = 50
+_fetch_limit=25
 
 def main(debug=False):
     '''main(debug=False)
@@ -66,8 +63,6 @@ def main(debug=False):
             log("Error: {}".format(e))
             time.sleep(_sleep_on_error)
 
-_seen_max_len = 50
-_fetch_limit=25
 # Returns true if anything happened
 def scan_submissions(seen, r):
     '''This function groups the following:
@@ -103,8 +98,8 @@ def scan_submissions(seen, r):
         log("Error during submissions scan: {}".format(e))
         return False
 
-# returns True if anything processed
 def process_mail(r):
+    '''Processes notifications.  Returns True if any item was processed.'''
     # log("Fetching unread mail.")
     my_mail = list(r.get_unread(unset_has_mail=False))
     to_process = [Request(x, r) for x in my_mail]
@@ -133,6 +128,7 @@ def process_mail(r):
     return ( 0 < len(to_process))
 
 def BeepBoop():
+    '''Builds and returns reply footer "Beep Boop I'm a bot..."'''
     s = "\n\n-----\n\n"
     s += ("*Beep boop I'm a bot.  " +
           "You can find details about me at " + 
@@ -150,356 +146,17 @@ def sign_in():
     return r
 
 def test(mens=True):
+    '''test(return_mentions=True)
+    if return_mentions, returns tuple (reddit_handle, list_of_all_mail, list_of_mentions)
+    else, returns tuple (reddit_handle, list_of_all_mail, None)
+    '''
     r = sign_in()
-    # already, ignore = pickle.load(open(_pickle_filename, 'rb'))
     my_mail = list(r.get_unread(unset_has_mail=False))
     if mens:
         mentions = list(r.get_mentions())
     else:
         mentions = None
     return r, my_mail, mentions
-
-def get_post_text(post):
-    '''Returns text to parse from either Comment or Submission'''
-    if type(post) == praw.objects.Comment:
-        return post.body
-    elif type(post) == praw.objects.Submission:
-        return post.selftext
-    else:
-        raise RuntimeError("Attempt to get post text from non-Comment / non-Submission post.")
-
-####################
-
-# A Request may refer to many TableSource objects
-# A TableSource may contain many tables
-# A table refers to many TableItems
-# A TableItem may contain an inline subtable
-# TODO: InlineTable should probably inherit from Table
-
-# All items parse text in constructor
-
-class Request:
-    def __init__(self, praw_ref, r):
-        self.origin = praw_ref
-        self.reddit = r
-        self.tables_sources = []
-        self.outcome = None
-        
-        self.parse()
-
-    def text(self):
-        if type(self.origin) == praw.objects.Comment:
-            return self.origin.body
-        elif type(self.origin) == praw.objects.Submission:
-            return self.origin.selftext
-        else:
-            return ""
-        
-    # determine params, identify targets
-    def parse(self):
-        # TODO(Parse params, change targets, pass to Table calls
-        T = TableSource(self.origin.submission, "this thread's original post")
-        if T.has_tables():
-            self.tables_sources.append(T)
-        top_level_comments = self.reddit.get_submission(None, self.origin.submission.id).comments
-        for item in top_level_comments:
-            T = TableSource(item, "[this]({}) comment by {}".format(item.permalink, item.author) )
-            if T.has_tables():
-                T.parse()
-                self.tables_sources.append(T)
-        
-    def roll(self):
-        instance = [TS.roll() for TS in self.tables_sources]
-        instance = [x for x in instance if x]
-        return "\n\n-----\n\n".join(instance)
-        
-    def reply(self, reply_text):
-        self.origin.reply(reply_text)
-
-    def is_summons(self):
-        return re.search(_summons_regex, get_post_text(self.origin).lower())
-
-    def log(self):
-        filename = "{}/{}-{}.log".format(_log_dir, self.origin.author, self.origin.fullname)
-        with open(filename, 'w') as f:
-            f.write("Time    :  {}\n".format(fdate() ))
-            f.write("Author  :  {}\n".format(self.origin.author))
-            f.write("Link    :  {}\n".format(self.origin.permalink))
-            f.write("Type    :  {}\n".format(type(self.origin)))
-            try:
-                f.write("Body    : (below)\n[Begin body]\n{}\n[End body]\n".format( get_post_text(self.origin)))
-            except:
-                f.write("Body    : Could not resolve message body.")
-            f.write("\n")
-            try:
-                f.write("Submission title : {}\n".format(self.origin.submission.title))
-                f.write("Submission body  : (below)\n[Begin selftext]\n{}\n[End selftext]\n".format(self.origin.submission.selftext))
-            except:
-                f.write("Submission: Could not resolve submission.")
-        filename = filename.rstrip("log") + "pickle"
-        with open(filename, 'wb') as f:
-            pickle.dump(self, f)
-
-    def describe_source(self):
-        return "From [this]({}) post by user {}...".format(self.source.permalink, self.source.author)
-
-    def __repr__(self):
-        return "<Request from /u/{} in thread \"{}\">".format(self.origin.author, self.origin.submission.title)
-
-class TableSource:
-    def __init__(self, praw_ref, descriptor):
-        self.source = praw_ref
-        self.desc = descriptor
-        self.tables = []
-
-        self.parse()
-        
-    def __repr__(self):
-        return "<TableSource from {}>".format(self.desc)
-
-
-    def roll(self):
-        instance = [T.roll() for T in self.tables]
-        instance = [x for x in instance if x]
-        if instance:
-            ret = "From {}...\n\n".format(self.desc)
-            for item in instance:
-                ret += item.unpack()
-            return ret
-        return None
-
-    def has_tables(self):
-        return ( 0 < len(self.tables) )
-
-    def parse(self):
-        indices = []
-        last_index = 0
-        text = get_post_text(self.source)
-        lines = text.split("\n")
-        for line_num in range(len(lines)):
-            l = lines[line_num]
-            if re.search(_header_regex, l.strip(_trash)):
-                indices.append(line_num)
-        # TODO: if no headers found
-        if len(indices) == 0:
-            return None
-        
-        table_text = []
-        for i in range(len(indices) -1):
-            table_text.append("\n".join(lines[ indices[i]:indices[i+1] ]))
-        table_text.append("\n".join(lines[ indices[-1]: ]))
-
-        self.tables = [ Table(t) for t in table_text ]
-
-class TableSourceFromText(TableSource):
-    def __init__(self, text, descriptor):
-        self.text = text
-        self.desc = descriptor
-        self.tables = []
-
-        self.parse()
-        
-    def parse(self):
-        indices = []
-        last_index = 0
-        text = self.text
-        lines = text.split("\n")
-        for line_num in range(len(lines)):
-            l = lines[line_num]
-            if re.search(_header_regex, l.strip(_trash)):
-                indices.append(line_num)
-        # TODO: if no headers found
-        if len(indices) == 0:
-            return None
-        
-        table_text = []
-        for i in range(len(indices) -1):
-            table_text.append("\n".join(lines[ indices[i]:indices[i+1] ]))
-        table_text.append("\n".join(lines[ indices[-1]: ]))
-
-        self.tables = [ Table(t) for t in table_text ]
-
-class Table:
-    '''Container for a single set of TableItem objects
-    A single post will likely contain many Table objects'''
-    def __init__(self, text):
-        self.text = text
-        self.die = None
-        self.header = ""
-        self.outcomes = []
-        self.is_inline = False
-
-        self.parse()
-
-    def __repr__(self):
-        return "<Table with header: {}>".format(self.text.split('\n')[0])
-
-    def roll(self):
-        try:
-            weights = [ i.weight for i in self.outcomes]
-            total_weight = sum(weights)
-            if debug:
-                print("Weights ; Outcome")
-                pprint(list(zip(self.weights, self.outcomes)))
-            assert self.die == total_weight, "Table roll error: parsed die did not match sum of item wieghts."
-            stops = [ sum(weights[:i+1]) for i in range(len(weights))]
-            c = random.randint(1, self.die)
-            scan = c
-            ind = -1
-            while scan > 0:
-                ind += 1
-                #print("Scanning: scan = {}; this weight = {}; index = {}".format(scan, weights[ind], ind))
-                scan -= weights[ind]
-                
-            R = TableRoll(d=self.die,
-                          rolled=c,
-                          head=self.header,
-                          out=self.outcomes[ind])
-            if len(self.outcomes) != self.die:
-                R.error("Expected {} items found {}".format(self.die, len(self.outcomes)))
-            return R
-        # TODO: Handle errors more gracefully.
-        except Exception as e:
-            log("Exception in Table roll ({}): {}".format(self, e))
-            return None
-
-    def parse(self):
-        lines = self.text.split('\n')
-        head = lines.pop(0)
-        head_match = re.search(_header_regex, head.strip(_trash))
-        if head_match:
-            self.die = int(head_match.group(2))
-            self.header = head_match.group(3)
-        self.outcomes = [ TableItem(l) for l in lines if re.search(_line_regex, l.strip(_trash)) ]
- 
-class TableItem:
-    '''This class allows simple handling of in-line subtables'''
-    def __init__(self, text, w=0):
-        self.text = text
-        self.inline_table = None
-        self.outcome = ""
-        self.weight = 0
-
-        self.parse()
-
-        # If parsing fails, particularly in inline-tables, we may
-        # explicitly set weights
-        if w:
-            self.weight = w
-
-    def __repr__(self):
-        return "<TableItem: {}{}>".format(self.outcome, "; has inline table" if self.inline_table else "")
-
-    def parse(self):
-        main_regex = re.search(_line_regex, self.text.strip(_trash))
-        if not main_regex:
-            return
-        # Grab outcome
-        self.outcome = main_regex.group(3).strip(_trash)
-        # Get weight / ranges
-        if not main_regex.group(2):
-            self.weight = 1
-        else:
-            try:
-                start = int(main_regex.group(1).strip(_trash))
-                stop = int(main_regex.group(2).strip(_trash))
-                self.weight = stop - start + 1
-            except:
-                self.weight = 1
-        # Identify if there is a subtable
-        if re.search("[dD]\d+", self.outcome):
-            die_regex = re.search("[dD]\d+", self.outcome)
-            try:
-                self.inline_table = InlineTable(self.outcome[die_regex.start():])
-            except RuntimeError as e:
-                log("Error in inline_table parsing ; table item full text:")
-                log(self.text)
-                log(e)
-            self.outcome = self.outcome[:die_regex.start()].strip(_trash)
-        # this might be redundant
-        self.outcome = self.outcome.strip(_trash)
-
-
-    def get(self):
-        if self.inline_table:
-            return self.outcome + self.inline_table.roll()
-        else:
-            return self.outcome
-        
-class InlineTable(Table):
-    def __init__(self, text):
-        super().__init__(text)
-        self.is_inline = True
-
-    def __repr__(self):
-        return "<d{} Inline table>".format(self.die)
-
-    def parse(self):
-        top = re.search("[dD](\d+)(.*)", self.text)
-        if not top:
-            return
-        
-        self.die = int(top.group(1))
-        tail = top.group(2)
-        sub_outs = []
-        while tail:
-            #print("tail =", tail)
-            in_match = re.search(_line_regex, tail.strip(_trash))
-            if not in_match:
-                raise RuntimeError("Could not complete parsing InlineTable; in_match did not catch.")
-            #print("in_match: g1 // {} // g2 // {} // g3 // {} //".format(
-            # in_match.group(1), in_match.group(2), in_match.group(3) )) 
-            this_out = in_match.group(3)
-            #print("this_out =", this_out)
-            # _line_regex[1:] drops line-start anchor
-            next_match = re.search(_line_regex[1:], this_out)
-            if next_match:
-                #print("next_match: g1 // {} // g2 // {} // g3 // {} //".format(
-                # next_match.group(1), next_match.group(2), next_match.group(3) )) 
-                tail = this_out[next_match.start():]
-                this_out = this_out[:next_match.start()]
-                #print("Updating tail =", tail,"; this_out =", this_out)
-            else:
-                tail = ""
-
-            TI_text = in_match.group(1) + (in_match.group(2) if in_match.group(2) else "") + this_out
-            try:
-                self.outcomes.append(TableItem(TI_text))
-            except Exception as e:
-                log("Error building TableItem in inline table; item skipped.")
-
-class TableRoll:
-    def __init__(self, d, rolled, head, out, err=None):
-        self.d = d
-        self.rolled = rolled
-        self.head = head
-        self.out = out
-        self.sub = out.inline_table
-        self.err = err
-
-        if self.sub:
-            self.sob_out = self.sub.roll()
-        
-    def __repr__(self):
-        return "<d{} TableRoll: {}>".format(self.d, self.head)
-
-    def error(self, e):
-        self.err = e
-
-    def unpack(self):
-        ret  = "{}...    \n".format(self.head.strip(_trash))
-        ret += "(d{} -> {}) {}.    \n".format(self.d, self.rolled, self.out.outcome)
-        if self.sub:
-            ret += "Subtable: {}".format(self.sub.roll().unpack())
-        ret += "\n\n"
-        return ret
-
-class Logger:
-    def __init__(self, log_directory, log_filename):
-        self.log_filename = log_filename
-
-class Core:
-    pass
 
 def log(s):
     if debug:
@@ -512,8 +169,6 @@ def log(s):
     
 ####################
 
-T = "This has a d12 1 one 2 two 3 thr 4 fou 5-6 fiv/six 7 sev 8 eig 9 nin 10 ten 11 ele 12 twe"
-T = "This has a d12 1 one 2 two 3 thr 4 fou 5-6 fiv/six 7 sev 8 eig 9 nin 10 ten 11 ele 12 twe"
 debug = ("y" in input("Enable debugging?  ").lower() )
 if __name__=="__main__":
     if 'y' in input("Run main?  ").lower():
