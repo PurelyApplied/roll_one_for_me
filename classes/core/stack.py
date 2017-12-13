2092dec451cc9dc7d3d10f3a7aa137ea2fad38a2
Patrick Rhomberg @ home <purelyapplied@gmail.com>
2017-12-12 20:38:52 -0800

    Stack stubbed out.  You realized that the way you have it set up,  you'll probably have to make sure that all the calls on the stack  return void (or whatever you want to call that in Python).
    Next up: make a ResponseContext to keep a table directory and
     abuse the hell out of its mutability to get around no return values.

--- classes/core/stack.py
+++ classes/core/stack.py
@@ -1,19 +1,55 @@
 #!/usr/bin/env python3
 from enum import Enum
 
-TOP_LEVEL_ACTION = Enum("answer_summons", "process_pms", "be_sentinel")
-ACTION = Enum("roll_table")
+
+class Command(str, Enum):
+    """This enum defines possible command strings when summoning."""
+    default = "default"
+    stats = "stats"
 
 
-class ActionStack:
-    def __init__(self):
-        pass
+class Action(str, Enum):
+    parse = "parse(target) -> table"
+    roll = "roll(table)"
+    add_table_to_table_set = "add_table_to_table_set(reflection, table)"
 
-    def add(self, descriptor, *arguments):
-        pass
 
-    def do(self):
-        pass
+def pack(__f, *args, **kwargs):
+    return __f, args, kwargs
 
-    def pop(self):
-        pass
+
+def call(__f, args, kwargs):
+    return __f(*args, **kwargs)
+
+
+def unpack(package):
+    assert isinstance(package, tuple) or isinstance(package, list) and len(package) == 3
+    return call(*package)
+
+
+class Stack(list):
+    def __init__(self, *actions: "Actions listed first execute first."):
+        """
+        Instance members:
+
+        self.stack contains either a tuple in the form (func, args, kwargs) or a TableAction.
+        TableAction objects are popped, translated to a (func, args, kwargs) tuple, and pushed back for processing.
+
+        """
+        super().__init__(*actions[::-1])
+        self.height = len(self)
+        self.depth = 0
+
+    def append(self, o):
+        super().append(o)
+        self.height += 1
+
+
+def example_print(a, b, c, d, e, f, g):
+    for char, val in zip('abcdefg', (a, b, c, d, e, f, g)):
+        print("{} = {}".format(char, val))
+
+if __name__ == '__main__':
+    unpack(pack(example_print, 1, 2, 3, 4, g=9, e=5, f=100))
+    stack = Stack(range(10, 0, -1))
+    print(stack)
