13ff3fe121f0bbdff0682ccac773bf7eace5e48f
Patrick Rhomberg <prhomberg@pivotal.io>
2017-12-28 20:43:56 -0600

    Moved parsers and utils to experimental

--- classes/__init__.py
+++ classes/__init__.py
@@ -1,2 +1,8 @@
-import classes.util
-import classes.reddit
+from experimental import parsers
+from . import core
+from . import reddit
+from . import rollers
+from . import tables
+from . import util
+
+__all__ = ["core", "parsers", "reddit", "rollers", "tables", "util", "utils"]
--- experimental/parsers/table_parser.py
+++ experimental/parsers/table_parser.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from classes.tables import Table
-from classes.utils.text import Text
+from experimental.text import Text
 
 
 def identify_tables(text: Text) -> list:
